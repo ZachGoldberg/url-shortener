3675194b1596aab9f9469005b68db7bfbaa5cc5b
Zach Goldberg <zach@wifast.com>
2013-02-04 18:44:54 -0800

    Include admin for login

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns(
     '',
     (r'^$', 'url_shortener.views.index'),
     (r'^submit/$', 'url_shortener.views.submit'),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^(?P<shortcut>\w+)$', 'url_shortener.views.follow'),
     (r'^info/(?P<shortcut>\w+)$', 'url_shortener.views.info'),
-
 )
